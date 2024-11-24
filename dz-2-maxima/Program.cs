a21b24572a4abf142d64765730505ff37e212c07
pollosatyi <pollosatyi@mail.ru>
2024-11-24 11:42:07 +0300

    Добавьте файлы проекта.

---
+++ dz-2-maxima/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace dz_2_maxima
+{
+    
+    internal class Program
+    {
+        public static List<List<string>> Chunked(List<string> list, int n)
+        {
+            // реализуйте функцию
+        }
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>() { "a","b","c","d","e","f" };
+            int n = 2;
+            var chunked = Chunked(list, n);
+        }
+    }
+}
